663db4eddb1d2f969b7eceb4a63f65f2a41bdc35
Gabriel <gabrielmcanterle@gmail.com>
2024-11-19 15:19:40 -0300

    Segunda aula

--- imersao-back-end/server.js
+++ imersao-back-end/server.js
@@ -1,20 +1,46 @@
 import express from 'express';
 
+const posts = [
+    {
+        id : 1,
+        descricao: "uma foto teste",
+        imagem : "https://placecats.com/millie/300/150"
+    },
+    {
+        id : 2,
+        descricao: "uma foto teste",
+        imagem : "https://placecats.com/millie/300/150"
+    },
+    {
+        id : 3,
+        descricao: "uma foto teste",
+        imagem : "https://placecats.com/millie/300/150"
+    }
+]
+
+
+
 const app = express();
+app.use(express.json());
 
 app.listen(3000,() => {
     (console.log("servidorEscutando"));
 });
 
-app.get("/api", (req,res) => {
-    res.status(200).send("Oi");
+app.get("/posts", (req,res) => {
+    res.status(200).send(posts);
 });
 
-app.get("/livro", (req,res) => {
-    res.status(200).json(
-    {titulo : "Senhor dos aneis",
-        autor : "J.R.R Tolkien",
-        ano : "1954",
-        genero: "fantasia"
-     });
+function buscarPostPorId(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    })
+};
+
+app.get("/posts/:id", (req,res) => {
+    const index = buscarPostPorId(req.params.id);
+    res.status(200).json(posts[index]);
 });
+
+//npm install mongodb
+//mongodb+srv://gabrielmcanterle:lalala@cluster0.7ux4x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
