07d7e604f251e7d8dc63a68625b1431a756bdf6c
Gabriel <gabrielmcanterle@gmail.com>
2024-11-21 15:32:04 -0300

    Quarta aula

---
+++ src/models/postsModel.js
@@ -0,0 +1,14 @@
+import conectarAoBanco from '../config/dbConfig.js';
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
+
+export async function getTodosPosts() {
+    const db = conexao.db("imersao-back-end");
+    const colecao = db.collection("posts");
+    return colecao.find().toArray();
+}
+
+export async function  criarNovoPost(novoPost) {
+    const db = conexao.db("imersao-back-end");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost)
+}
